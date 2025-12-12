695cdb0e221f7d0196b29be0cd6198808f2ddba4
Rajko Lapcevic <rajkol581@gmail.com>
2025-12-12 22:50:33 +0100

    Revert "Add simple extension that collects data from google search input bar with permisions of users"
    This reverts commit da9fad1540c8842d7d16115ac6f1294c7cf8db8e.

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg === "check_permission") {
-    chrome.permissions.contains(
-      { origins: ["https://www.google.com/*"] },
-      (result) => sendResponse(result)
-    );
-    return true;
-  }
-});
--- popup.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("allow").addEventListener("click", async () => {
-  const granted = await chrome.permissions.request({
-    origins: ["https://www.google.com/*"]
-  });
-
-  document.getElementById("status").innerText = granted
-    ? "Dozvola odobrena."
-    : "Dozvola odbijena.";
-});
